718fc3ab4ca2e85f35b32217ce626631add56d03
Kacper Kapuściak <39658211+kacperkapusciak@users.noreply.github.com>
2019-05-10 21:50:21 +0200

    Routing setup (#1)
    * Added react-router-dom package
    
    * Routing
    
    * Components boilerplate

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Route, Switch, withRouter, Redirect } from "react-router-dom";
+import RouteInfo from "./components/RouteInfo/RouteInfo";
+import Vehicles from "./components/Vehicles/Vehicles";
+import Details from "./components/Details/Details";
+
+function App() {
+  return (
+    <>
+      <Switch>
+        <Route path="/when" component={RouteInfo} />
+        <Route path="/what" component={Vehicles} />
+        <Route path="/details-and-payment" component={Details} />
+        <Route path="/" exact component={RouteInfo} />
+        <Redirect to="/" />
+      </Switch>
+    </>
+  );
+}
+
+export default withRouter(App);
