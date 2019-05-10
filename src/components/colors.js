1941911ecc44479bb18ca0beef972abc54fe77c1
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-05-10 22:43:10 +0200

    Added styled-components and semantic-ui-css packages

---
+++ src/components/colors.js
@@ -0,0 +1,4 @@
+export const primary = "#4c4c4c";
+export const secondary = "#1e2731";
+export const accent = "#ff4800";
+export const background = "#d2d6d9";
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import 'semantic-ui-css/semantic.min.css'
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 const app = (
   <BrowserRouter basename="/booking">
-    <App />
+    <App/>
   </BrowserRouter>
 );
 
