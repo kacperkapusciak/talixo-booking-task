feab286a99064a86a887f4675f4237fcc3b1690d
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-05-10 21:44:09 +0200

    routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = (
+  <BrowserRouter basename="/booking">
+    <App />
+  </BrowserRouter>
+);
+
+ReactDOM.render(app, document.getElementById("root"));
