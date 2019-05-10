1cdde95b17adbfe979f9a1f9b7c746e6cf04123c
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-05-10 23:35:02 +0200

    Implemented Header and Logo components

---
+++ src/components/UI/Logo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+import logo from "./talixo_logo_2x.png";
+
+export default () => (
+  <Img src={logo} alt="talixo logo"/>
+);
+
+const Img = styled.img`
+  height: 22px;
+  width: 123px;
+  margin-top: 7px;
+`;
---
+++ src/components/UI/breakpoints.js
@@ -0,0 +1,4 @@
+export const sm = "600px";
+export const md = "768px";
+export const lg = "1024px";
+export const xlg = "1200px";
